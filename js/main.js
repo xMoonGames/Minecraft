032077635cc92a6ff701715c785ff34a5a17ab31
RenanMsV <r.delaneza@gmail.com>
2021-09-13 05:45:22 -0300

    Customized for xMoonGames

--- js/main.js
+++ js/main.js
@@ -71,7 +71,7 @@ const updatePlayercount = (ip, port) => {
         if (result.hasOwnProperty('online')) {
             $(".sip").html(result.online);
         } else {
-            $(".playercount").html("Server isn't online!");
+            $(".playercount").html("O servidor parece estar desligado neste momento");
         }
     });
 };
